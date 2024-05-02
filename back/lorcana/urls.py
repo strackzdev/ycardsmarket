0a53079e05d1801883dcf447fa0e8075e13aa64d
Strackz <strackzdesign@gmail.com>
2024-05-02 10:18:15 +0200

    feat: lorcana collections and serialisers

--- back/lorcana/urls.py
+++ back/lorcana/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get),
-    path("<int:card_number>/seasons/<int:set_num>", views.get_by_card_number_and_set_number),
+    path("", views.get, name="lorcana-cards"),
+    path("<int:card_number>/seasons/<int:set_num>", views.get_by_card_number_and_set_number, name='lorcana-card'),
+    path("collections", views.create_lorcana_collection, name='lorcana-collection'),
+    path("collections/<int:collection_id>", views.handle_collection_by_id, name='handle-collection-by-id'),
 ]
