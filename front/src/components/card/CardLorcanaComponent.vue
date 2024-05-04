2151756584ff07cc9685693f4ae2aa5c9d7b7563
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-05-04 02:22:39 +0200

    implement (better) rotation and scaling card and center vertically

--- front/src/components/card/CardLorcanaComponent.vue
+++ front/src/components/card/CardLorcanaComponent.vue
@@ -1,29 +1,35 @@
 <template>
-    <img class="rounded-xl" :class="[isRotated ? 'rotate-90 scale-[0.715]' : '']" :src="props.card.image_url" alt="">
+  <div class="flex flex-col justify-center">
+    <img
+      class="rounded-xl w-fit"
+      :class="[isRotated ? 'rotate-90 aspect-square scale-x-75' : '']"
+      :src="props.card.image_url"
+      alt=""
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { type CardLorcana } from './CardInterface';
-import { CardLorcanaType } from './CardLorcanaEnum';
+import { onMounted, ref } from 'vue'
+import { type CardLorcana } from './CardInterface'
+import { CardLorcanaType } from './CardLorcanaEnum'
 
 // Props
-const props = defineProps<{card : CardLorcana}>();
+const props = defineProps<{ card: CardLorcana }>()
 
 // Refs
-const isRotated = ref(false);
+const isRotated = ref(false)
 
 // Hook
 onMounted(() => {
-    doRotation();
+  doRotation()
 })
 
 // Functions
 function doRotation() {
-    if (props.card.type === CardLorcanaType.LOCATION) {
-        console.log(props.card.type)
-        isRotated.value = !isRotated.value
-    }
+  if (props.card.type === CardLorcanaType.LOCATION) {
+    console.log(props.card.type)
+    isRotated.value = !isRotated.value
+  }
 }
-
 </script>
