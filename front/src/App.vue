3710d519e45df6934d0c12b707212326c57bb57f
Ssonyan Dumas-H <ssonyan.dh@gmail.com>
2024-06-12 07:22:38 -0700

    feat: refacto Header

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,7 +3,7 @@ import {RouterLink, RouterView} from 'vue-router'
 import NavCards from '../src/components/NavCards.vue'
 import HomeView from '@/views/HomeView.vue'
 import Button from "@/components/homePage/HomeButton.vue";
-import MenuItems from "@/components/homePage/MenuItems.vue";
+import MenuItems from "@/components/homePage/Header.vue";
 </script>
 
 <template>
