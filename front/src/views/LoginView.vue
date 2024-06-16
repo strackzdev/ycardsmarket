ccbfedd2cfadfae606660e265f51d9db93746347
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-16 13:00:11 +0200

    feat: implement hardcoded login

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -12,16 +12,16 @@
               <form class="space-y-4 md:space-y-6" action="#">
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium navy-blue">Email or username</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 navy-blue sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="player@ycardmarket.com or username" required="">
+                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 navy-blue sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="player@ycardmarket.com or username" required>
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium navy-blue">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 navy-blue sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="">
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 navy-blue sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required>
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
                           <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required>
                           </div>
                           <div class="ml-3 text-sm">
                             <label for="remember" class="barbel-blue">Remember me</label>
@@ -29,7 +29,7 @@
                       </div>
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                  <button @click="login" type="submit" class="py-2 w-full navy-blue-bg text-white rounded-lg">Sign in</button>
                   <p class="text-sm font-light barbel-blue">
                       Don’t have an account yet? <a href="#" class="font-bold navy-blue text-primary-600 hover:underline">Sign up</a>
                   </p>
@@ -40,4 +40,26 @@
 </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+
+const login = () => {
+  const data = { 
+    username: 'app_user',
+    password: 'app_user_password',
+    grant_type: 'password',
+    client_id: 'trady-go-login',
+  };
+
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data,
+    url: 'https://vps.strackzdev.com/realms/tradygo-realm/protocol/openid-connect/token'
+  }
+
+  axios(options)
+  .then(res => localStorage.setItem('token', res.data.access_token))
+  .catch(error => console.log(error))
+};
+</script>
