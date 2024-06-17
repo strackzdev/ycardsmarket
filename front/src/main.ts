e5116969248910dc755e6405312f4b77a6daaf20
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-17 22:29:06 +0200

    feat: add implement Bearer token interceptor and backend-proxy to avoid CORS in dev

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,17 +1,23 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
 
 const app = createApp(App)
-
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (localStorage.getItem('token')) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 app.mount('#app')
--- front/src/views/DecksView.vue
+++ front/src/views/DecksView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="text-green-700 text-xl">DECKS</div>
+  {{ cardgames }}
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import axios from 'axios'
+import { ref } from 'vue';
+
+  const cardgames = ref<string>();
+
+  axios.get('backend/cardgames').then(res => {
+    cardgames.value = res.data
+  })
+
+</script>
