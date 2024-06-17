88e1912f3bcccc0904b549f016025741b2e290ee
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-17 22:29:06 +0200

    feat: add implement Bearer token interceptor and backend-proxy to avoid CORS in dev

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,16 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/backend-proxy": {
+        target: "https://vps.strackzdev.com:8443/api/v1",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/backend-proxy/, "")
+      }
+    }
+  },
   plugins: [
     vue(),
     VueDevTools(),
