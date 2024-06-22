0238307004341b99d4e3a1fcbc10816b313db783
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-22 02:18:50 +0200

    refactor: interceptors

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,9 +4,7 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 import { createPinia } from 'pinia'
-import axios, { type InternalAxiosRequestConfig, AxiosError } from 'axios'
-import { logout } from './auth/auth'
-import { getAccessToken, renewAccessToken } from './auth/token'
+import { setAccessTokenInterceptorRequest, handleExpiredTokenInterceptorResponse } from './interceptors/token'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -14,36 +12,7 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 
-axios.interceptors.request.use((config) => {
-  const access_token = getAccessToken();
-  if (access_token) {
-    config.headers.Authorization = `Bearer ${access_token}`;
-  }
-  return config;
-})
-
-axios.interceptors.response.use(
-  (res) => { return res },
-  async (error: AxiosError) => {
-    const currentRequest: InternalAxiosRequestConfig<any> | undefined = error.config
-
-    if(error.response && error.response.status === 401 && currentRequest) {
-      try {
-        const access_token = await renewAccessToken();
-        currentRequest.headers['Authorization'] = `Bearer ${access_token}`;
-
-        return await axios(currentRequest);
-      } catch (e) {
-        await logout();
-
-        router.push({name:'login'});
-
-        return Promise.reject('Unauthorized');
-      }
-    } 
-
-    return Promise.reject(error);
-  }
-)
+setAccessTokenInterceptorRequest();
+handleExpiredTokenInterceptorResponse();
 
 app.mount('#app')
