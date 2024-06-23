ce5a94626e7ec5a4d843322b730568196637ec37
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-23 13:34:32 +0200

    feat: styling search button and dropdown hover

--- front/src/components/utils/FilterDropdownComponent.vue
+++ front/src/components/utils/FilterDropdownComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <select class="px-4 h-7 md:h-14 navy-blue italic border-2 barbel-blue-border bg-white" @change="updateSelect">
+  <select class="px-4 h-7 md:h-14 navy-blue italic border-2 barbel-blue-border barbel-blue-bg-lighter-hover bg-white" @change="updateSelect">
     <option value="">{{ defaultValue }}</option>
     <option disabled>──────────</option>
     <option v-for="[key, value] in cardLorcanaTypes" :key="key" :value="value">{{ value }}</option>
--- front/src/components/utils/SearchBarComponent.vue
+++ front/src/components/utils/SearchBarComponent.vue
@@ -7,6 +7,7 @@
         </svg>
     </div>
     <input @input="updateSearch" :value="searchInput" type="search" id="default-search" class="cursor-text h-14 navy-blue-bg block w-full placeholder-white text-white p-4 ps-10 text-sm rounded-full" placeholder="Search your card" />
+    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 barbel-blue-border barbel-blue-bg barbel-blue-bg-darker-hover font-medium rounded-full text-sm mr-5 px-4 py-2">Search</button>
 </div>
 
 
