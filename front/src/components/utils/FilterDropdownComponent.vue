0fc76f3d0034e93482efa15abd8841d91e65cf67
Cyril Nguyen <nguyencyril@hotmail.fr>
2024-06-24 21:49:24 +0200

    fix: remove long word overflow on arrow icon

--- front/src/components/utils/FilterDropdownComponent.vue
+++ front/src/components/utils/FilterDropdownComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <select class="px-4 h-7 md:h-14 navy-blue italic border-2 barbel-blue-border barbel-blue-bg-lighter-hover bg-white" @change="updateSelect">
+  <select class="pl-5 pr-14 h-7 md:h-14 rounded-full navy-blue border-2 barbel-blue-border barbel-blue-bg-lighter-hover bg-white" @change="updateSelect">
     <option value="">{{ props.filterName }}</option>
-    <option disabled>──────────</option>
+    <option disabled>──</option>
     <option v-for="[key, value] in options" :key="key" :value="value">{{ value }}</option>
   </select>
 </template>
