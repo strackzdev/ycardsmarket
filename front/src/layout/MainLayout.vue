bbdcb3a6c599e59d4905566ca1e339a4b9c23055
timothée picard <timothee.picard@outlook.fr>
2024-06-29 13:09:23 +0200

    feat: create view confirm modal

--- front/src/layout/MainLayout.vue
+++ front/src/layout/MainLayout.vue
@@ -1,10 +1,11 @@
 <template>
-    <Header/>
-    <RouterView/>
+  <Header/>
+  <RouterView/>
+  <ConfirmModal />
 </template>
 
 <script setup lang="ts">
     import { RouterView } from 'vue-router'
     import Header from '@/layout/LoginHeader.vue';
-    import NavigationCards from '@/layout/NavigationCards.vue';
+    import ConfirmModal from '@/components/modal/ConfirmModal.vue'
 </script>
