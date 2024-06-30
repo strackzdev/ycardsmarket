c0e2e5471d25947b6d7e56f850d583f69ac5c788
timothée picard <timothee.picard@outlook.fr>
2024-06-30 18:32:10 +0200

    feat: import in global layout

--- front/src/layout/MainLayout.vue
+++ front/src/layout/MainLayout.vue
@@ -2,11 +2,12 @@
   <Header/>
   <RouterView/>
   <ConfirmModal />
+  <ToastNotification />
 </template>
 
 <script setup lang="ts">
     import { RouterView } from 'vue-router'
     import Header from '@/layout/LoginHeader.vue';
     import ConfirmModal from '@/components/modal/ConfirmModal.vue'
-    import NavigationCards from '@/layout/NavigationCards.vue';
+    import ToastNotification from '@/components/toast/ToastNotification.vue'
 </script>
